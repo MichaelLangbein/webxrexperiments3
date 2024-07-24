ef26fd576d24d778b670775b8dd5e5e2e4b25b37
michaellangbein <michael_langbein@hotmail.de>
2024-07-24 16:03:37 +0200

    simple ar scene

--- src/main.ts
+++ src/main.ts
@@ -9,59 +9,55 @@ import {
     Vector3,
     WebGLRenderer,
 } from 'three';
-import { OrbitControls as Controlls } from 'three/examples/jsm/Addons.js';
+import { ARButton } from 'three/examples/jsm/Addons.js';
 
 /**
- * Threejs: camera per default looks into negative x direction
+ * Threejs:
+ *  - camera per default looks into negative z direction
+ *  - assuming camara starts off being held out horizontally:
+ *  - z: out of the screen, x: right, y: up
+ *  -
+ *
  */
 
+const container = document.getElementById('app') as HTMLDivElement;
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
 const renderer = new WebGLRenderer({
-    alpha: false,
+    alpha: true,
     antialias: true,
     canvas: canvas,
 });
+renderer.xr.enabled = true;
+
+const arb = ARButton.createButton(renderer, {
+    // type: https://immersive-web.github.io/webxr/#feature-dependencies
+});
+
+container.appendChild(arb);
 
 const scene = new Scene();
 
 const camera = new PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 100);
-camera.position.set(-10, 0, 0);
-camera.lookAt(new Vector3(0, 0, 0));
-// scene.add(camera);
-
-const controls = new Controlls(camera, canvas);
-
-const ah = new AxesHelper(3);
-ah.position.set(0, 0, 0);
-scene.add(ah);
+camera.position.set(0, 1.6, 0);
 
 const cube = new Mesh(
-    new BoxGeometry(1, 1, 1, 2, 2, 2),
+    new BoxGeometry(0.2, 0.2, 0.2, 2, 2, 2),
     new MeshBasicMaterial({
         color: 'green',
     })
 );
-cube.position.set(2, 2, 0);
+cube.position.set(0, 1.6, -2);
 scene.add(cube);
 
 const light = new PointLight('white', 1);
 scene.add(light);
 
-const loopTime = 30;
 function loop() {
-    const startTime = new Date().getTime();
-
-    controls.update(loopTime);
-    cube.rotateOnAxis(new Vector3(1, 1, 1), 0.01);
+    cube.rotateY(0.01);
     renderer.render(scene, camera);
-
-    const endTime = new Date().getTime();
-    const timePassed = endTime - startTime;
-    const timeLeft = loopTime - timePassed;
-    setTimeout(() => requestAnimationFrame(loop), timeLeft);
 }
 
-loop();
+renderer.setAnimationLoop(loop);
