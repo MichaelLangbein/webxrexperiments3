42751e95d605aa8ade2f9daa357aecb18c50b33d
michaellangbein <michael_langbein@hotmail.de>
2024-07-24 16:41:23 +0200

    started work on reticle

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,25 @@
 import {
     AxesHelper,
     BoxGeometry,
+    Camera,
     Mesh,
     MeshBasicMaterial,
+    Object3D,
     PerspectiveCamera,
     PointLight,
+    Raycaster,
+    RingGeometry,
     Scene,
+    Vector2,
     Vector3,
     WebGLRenderer,
 } from 'three';
 import { ARButton } from 'three/examples/jsm/Addons.js';
 
 /**
+ * https://threejs.org/manual/#en/webxr-look-to-select
+ * https://github.com/mrdoob/three.js/blob/master/examples/webxr_ar_hittest.html#L120
+ *
  * Threejs:
  *  - camera per default looks into negative z direction
  *  - assuming camara starts off being held out horizontally:
@@ -30,10 +38,12 @@ const renderer = new WebGLRenderer({
     antialias: true,
     canvas: canvas,
 });
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.xr.enabled = true;
 
 const arb = ARButton.createButton(renderer, {
     // type: https://immersive-web.github.io/webxr/#feature-dependencies
+    requiredFeatures: ['hit-test'],
 });
 
 container.appendChild(arb);
@@ -52,10 +62,42 @@ const cube = new Mesh(
 cube.position.set(0, 1.6, -2);
 scene.add(cube);
 
+const reticle = new Mesh(new RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2), new MeshBasicMaterial());
+// we will calculate position and rotation manually on every frame
+reticle.matrixAutoUpdate = false;
+reticle.visible = false; // only made visible when hitting wall or floor
+scene.add(reticle);
+
 const light = new PointLight('white', 1);
 scene.add(light);
 
-function loop() {
+const controller = renderer.xr.getController(0);
+controller.addEventListener('select', (evt) => {
+    console.log(evt);
+});
+
+let hitTestSourceRequested = false;
+async function loop(time: DOMHighResTimeStamp, frame: XRFrame) {
+    let hitTestSource: XRHitTestSource | undefined;
+
+
+    const referenceSpace = renderer.xr.getReferenceSpace() as XRReferenceSpace;
+    const session = renderer.xr.getSession() as XRSession;
+
+    if (hitTestSourceRequested === false) {
+        const viewerReferenceSpace = await session.requestReferenceSpace('viewer');
+        hitTestSource = (await session.requestHitTestSource!({ space: viewerReferenceSpace })) as XRHitTestSource;
+        session.addEventListener('end', () => {
+            hitTestSourceRequested = false;
+            hitTestSource = undefined;
+        });
+    }
+  
+
+    if (hitTestSource) {
+      const hitTestResults = frame.
+    }
+
     cube.rotateY(0.01);
     renderer.render(scene, camera);
 }
